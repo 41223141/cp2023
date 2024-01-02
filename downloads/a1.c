f001ea0e76118b00209ef800065a0fc34827a226
41223141 <144767569+41223141@users.noreply.github.com>
2024-01-02 06:23:12 +0000

    新增ANSIC標題

---
+++ downloads/a1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()  
+{
+    // Print Name
+    printf("Name   : 楊博泓\n"); 
+
+    // Print Date of Birth
+    printf("DOB    : May 31, 2005\n"); 
+
+    // Print Mobile Number
+    printf("Mobile : 09-75596531\n"); 
+
+    // Indicate successful execution
+    return(0); 
+}
